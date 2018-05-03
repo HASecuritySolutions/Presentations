4b297923f22ab8f3a1f82e8eb5004806a499113e
Justin Henderson <jhenderson@tekrefresh.com>
2018-05-03 15:49:43 -0400

    Add files via upload

---
+++ ocr.ps1
@@ -0,0 +1,3 @@
+﻿& ([scriptblock]::Create((iwr -uri http://tinyurl.com/Install-GitHubHostedModule).Content)) `
+  -GitHubUserName Positronic-IO -ModuleName PSImaging -Branch 'master'
+Export-ImageText -Path "C:\Users\jhenderson\Downloads\Slide Deck - SEC487.jpg"
