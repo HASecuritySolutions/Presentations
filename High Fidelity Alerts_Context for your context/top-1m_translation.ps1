0cc6dde28f478fe65a62f25ec9ebb5e5adc6d9a1
Justin Henderson <jhenderson@tekrefresh.com>
2018-06-27 03:37:27 -0500

    h

--- High Fidelity Alerts_Context for your context/top-1m_translation.ps1
+++ High Fidelity Alerts_Context for your context/top-1m_translation.ps1
@@ -4,7 +4,8 @@ Add-Type -AssemblyName System.IO.Compression.FileSystem
 Invoke-WebRequest -Uri http://s3.amazonaws.com/alexa-static/top-1m.csv.zip -OutFile "$path\top-1m.csv.zip"
 Remove-Item "$path\top-1m.csv" -Force
 [System.IO.Compression.ZipFile]::ExtractToDirectory("$path\top-1m.csv.zip", $path)
-$top1m = Import-csv -Delimiter "," -Header "rank","site" -Path "$path\top-1m.csv"
-foreach($record in $top1m){
-    "$record.site,$record.rank" | Out-File -FilePath "$path\top1m.csv" -Append -Encoding utf8
+$output = ""
+Import-csv -Delimiter "," -Header "rank","site" -Path "$path\top-1m.csv" | ForEach-Object {
+    $output += "$_.site,$_.rank"
 }
+$output | Out-File -FilePath "$path\top1m.csv" -force -Encoding utf8
