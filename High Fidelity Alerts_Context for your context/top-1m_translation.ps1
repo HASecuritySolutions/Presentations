38d716ff9ba262aa460d8ab8be851e7899c5f12e
Justin Henderson <jhenderson@tekrefresh.com>
2018-06-27 03:06:16 -0500

    f
    f

---
+++ High Fidelity Alerts_Context for your context/top-1m_translation.ps1
@@ -0,0 +1,10 @@
+﻿$path = "C:\Users\jhenderson\Downloads"
+
+Add-Type -AssemblyName System.IO.Compression.FileSystem
+Invoke-WebRequest -Uri http://s3.amazonaws.com/alexa-static/top-1m.csv.zip -OutFile "$path\top-1m.csv.zip"
+Remove-Item "$path\top-1m.csv" -Force
+[System.IO.Compression.ZipFile]::ExtractToDirectory("$path\top-1m.csv.zip", $path)
+$top1m = Import-csv -Delimiter "," -Header "rank","site" -Path "$path\top-1m.csv"
+foreach($record in $top1m){
+    "$record.site,$record.rank" | Out-File -FilePath top1m.csv -Append -Encoding utf8
+}
