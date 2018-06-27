52c48e9728aae1cfbb711037fcdb47f3a35accf8
Justin Henderson <jhenderson@tekrefresh.com>
2018-06-27 02:46:00 -0500

    c

---
+++ High Fidelity Alerts_Context for your context/sid_translation.ps1
@@ -0,0 +1,9 @@
+﻿#$rules = get-content -Path C:\Users\jhenderson\Downloads\rules\*.rules | Where-Object { $_ -notmatch "^#" }
+$output = "{"
+
+foreach($rule in $rules){
+    if($sid = [regex]::match($rule, "sid:(?<sid>[0-9]+);").Groups[1].Value){
+        $modified_rule = $rule -replace "'","" -replace '"','' -replace ",",""
+        "$($sid),$($modified_rule)" | Out-File -FilePath rules.csv -Append -Encoding utf8
+    }
+}
